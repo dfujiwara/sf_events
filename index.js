018cf55f6ea3a276ba341e79a36e267f871cb4f0
Daisuke Fujiwara <daisuke.fujiwara@gmail.com>
2019-06-01 08:54:38 -0700

    Initial email code (#8)

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 const needle = require('needle');
 const ogs = require('open-graph-scraper')
+const gmailSend = require('gmail-send')
+const config = require('./config')
 
 class Event {
   constructor(eventData, domain= 'https://www.sfjazz.org') {
     this.name = eventData.name
     this.date = new Date(eventData.eventDate)
     this.link = `${domain}${eventData.detailsLink}`
-  }
-
-  get summary() {
-    return `${this.name} at ${this.date.toDateString()}. More details at ${this.link}`
   }
 }
 
@@ -18,6 +16,7 @@ class OpenGraph {
     this.title = openGraphData.data.ogTitle  
     this.url = openGraphData.data.ogUrl
     this.image = openGraphData.data.ogImage.url
+    this.description = openGraphData.data.ogDescription
   }
 }
 
@@ -25,14 +24,19 @@ const run = async () => {
   try {
     const eventData = await fetchListing()
     const events = dedupeEvents(eventData.map((data) => new Event(data)))
-    events.forEach(async (event) => {
-      try {
-        const result = await fetchEventPageOpenGraphData(event)
-        console.log(result)
-      } catch (error) {
-        console.error(error)
-      }
-    })
+    const openGraphPromises = events
+      .map(async (event) => {
+        try {
+          return [event, await fetchEventPageOpenGraphData(event)]
+        } catch (error) {
+          console.error(error)
+          return null
+        }
+      })
+    const openGraphEvents = await Promise.all(openGraphPromises)
+    const filteredOpenGraphEvents = openGraphEvents.filter((tuple) => tuple !== null)
+    const generatedHTML = generateHTML(filteredOpenGraphEvents)
+    email('SF JAZZ!', generatedHTML)
   } catch(error) {
     console.error(error)
     return
@@ -71,6 +75,7 @@ const dedupeEvents = (events) => {
   const linkSet = new Set()
   return events.filter((event) => {
     if (linkSet.has(event.link)) {
+      console.log(`deduping ${event.link}`)
       return false
     }
     linkSet.add(event.link)
@@ -78,4 +83,47 @@ const dedupeEvents = (events) => {
   })
 }
 
+const generateHTML = (openGraphEvents) => {
+  const divCollection = openGraphEvents
+    .map((openGraphEvent) => {
+      const [event, openGraph] = openGraphEvent
+      return `
+        <div>
+          <a href='${event.link}'>${event.name}</a> - ${event.date}
+          <img src='${openGraph.image}'>
+        </div>`
+    })
+    .reduce((html, eventDiv) => html + eventDiv, '')
+  return `
+  <!doctype html>
+  <html>
+    <head>
+    </head>
+    <body>
+      ${divCollection}
+    </body>
+  </html>
+  `
+}
+
+const email = (subject, html, recipients = config.recipients, emailUserName = config.emailUserName, password = config.password) => {
+  const sendEmail = gmailSend({
+    user: emailUserName,
+    pass: password,
+    subject: subject,
+    html: html
+  })
+
+  console.log(html)
+  recipients.map(recipient => {
+    console.log(recipient)
+    sendEmail({ to: recipient }, (err, res) => {
+      if (err != null) {
+        console.error(err)
+      } else {
+        console.log(res)
+      }
+    })
+  })
+}
 run()
