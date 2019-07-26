fef41f1be8f7c0f9e6b57ac797a98f9b900033bc
Daisuke Fujiwara <daisuke.fujiwara@gmail.com>
2019-07-25 23:07:50 -0700

    Make the typescript more object oriented (#15)

--- src/event.ts
+++ src/event.ts
@@ -36,3 +36,35 @@ export class OpenGraph {
         this.description = openGraphData.ogDescription
     }
 }
+
+export interface EventSource {
+    name: string
+    fetchListing(date: Date): Promise<[Event, OpenGraph][]>
+}
+
+export function generateHTML(eventSource: EventSource, openGraphEvents: [Event, OpenGraph][]) {
+  const divCollection = openGraphEvents
+    .map((openGraphEvent) => {
+      const [event, openGraph] = openGraphEvent
+      return `
+        <div>
+          <a href='${event.link}'>${event.name}</a> - <time>${event.date}</time>
+          <div>${openGraph.description || 'No description'}</div>
+          <div>
+            <img src='${openGraph.image}'/>
+          </div>
+        </div>`
+    })
+    .reduce((html, eventDiv) => html + eventDiv, '')
+  return `
+  <!doctype html>
+  <html>
+    <head>
+    </head>
+    <body>
+      <h1>${eventSource.name}</h1
+      ${divCollection}
+    </body>
+  </html>
+  `
+}
