ccd20e4a1b3dcc89b1ba8fbf9197c63ed21c47b8
Daisuke Fujiwara <daisuke.fujiwara@gmail.com>
2020-08-22 14:52:27 -0700

    Apply the correct publication date for the event structure
    From SFGate dining RSS feed.

--- src/sf-gate-dining.ts
+++ src/sf-gate-dining.ts
@@ -2,6 +2,9 @@ import * as ogs from 'open-graph-scraper'
 import * as Parser from 'rss-parser'
 import { OpenGraph, EventSource, Event } from 'event'
 
+type RSSListing = [string, Date]
+type OpenGraphWithDate = OpenGraph & { date: Date }
+
 export class SFGateDining implements EventSource {
   public name = 'SF Gate Dining'
   private rssURL = 'https://www.sfgate.com/rss/feed/Food-Dining-550.php'
@@ -12,29 +15,28 @@ export class SFGateDining implements EventSource {
     return this.createEvents(openGraphDataList)
   }
 
-  private async fetchRSS(): Promise<string[]> {
+  private async fetchRSS(): Promise<RSSListing[]> {
     const parser = new Parser()
     const feed = await parser.parseURL(this.rssURL)
-    return feed.items.map(item => item.link)
+    return feed.items.map(item => [item.link, new Date(item.pubDate)])
   }
 
-  private async fetchOpenGraph(links: string[]): Promise<OpenGraph[]> {
-    const promises = links.map(async link => {
+  private async fetchOpenGraph(listings: RSSListing[]): Promise<OpenGraphWithDate[]> {
+    const promises = listings.map(async ([link, pubDate]) => {
       const options = { url: link }
       const result = await ogs(options)
-      const openGraph = result.data
-      return openGraph
+      return { date: pubDate, ...result.data }
     })
     return Promise.all(promises)
   }
 
-  private createEvents(openGraphDataList: OpenGraph[]): Event[] {
+  private createEvents(openGraphDataList: OpenGraphWithDate[]): Event[] {
     return openGraphDataList.map(data => ({
       name: data.ogTitle,
       link: data.ogUrl,
       image: data.ogImage.url,
       description: data.ogDescription,
-      date: new Date(),
+      date: data.date,
     }))
   }
 }
